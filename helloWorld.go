6d540b89ab6109e2cbcf7e0e65eb2891889f122a
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-01-19 22:10:43 +0100

    First practice

---
+++ helloWorld.go
@@ -0,0 +1,23 @@
+package main
+
+
+func main(){
+	//fmt.Println("Hello World \nI love to code" )
+	//var s string
+	//
+	//s = "iseoluwa"
+	//fmt.Println(s)
+	//s = "fasoyin"
+	//fmt.Println(s)
+	//
+	//s = s + " comfort"
+	//fmt.Println(s)
+	//
+	//s +=" oluwapelumi"
+	//fmt.Println(s)
+	//
+	//fmt.Println(321325*424521)
+
+	variable()
+
+}
---
+++ variables.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func variable() {
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+	output := input * 2
+	fmt.Println(output)
+}
