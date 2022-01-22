fa0b7d1e2f1b82982990348bd67fdc5c61257bd5
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-01-22 22:45:35 +0100

    practice

--- map.go
+++ map.go
@@ -1,5 +1,41 @@
 package main
+import "fmt"
 
-func main() {
-	$END$
+func maps() {
+	var x =make(map[string]int)
+	x["key"] = 10
+	fmt.Println(x["key"])
+	//to delete
+	delete(x, "key")
+}
+
+//first 20 elements
+func elements(){
+	elements := make(map[string]string)
+	elements["H"]= "hydrogen"
+	elements["He"]= "helium"
+	elements["Li"]= "Lithium"
+	fmt.Println(elements["Li"])
+}
+
+//modified version
+func mapModified(){
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+	}
+
+	if el, ok := elements["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
 }
