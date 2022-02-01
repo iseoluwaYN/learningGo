367b4aacff4d2e7c7d718e7dc936cc1b40ef4366
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-01 02:51:58 +0100

    channel(sending and recieving)

--- concurrency/channels.go
+++ concurrency/channels.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	$END$
+	var c chan string= make(chan string)
+
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
+}
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+func printer(c chan string) {
+	for {
+		msg := <- c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
 }
